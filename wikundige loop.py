d529b66d4435e8807c6e5b105870dc870312aac0
pieterPDG <51637168+pieterPDG@users.noreply.github.com>
2019-06-12 12:14:23 +0200

    Add files via upload

---
+++ wikundige loop.py
@@ -0,0 +1,21 @@
+import os
+your_number1 = float(input("geef mij een nummer : "))
+your_number2 = float(input("geef mij een nummer: "))
+ja_neen = input("zijn je nummers  " + str(your_number1) + " en " + str(your_number2) + " ?\nantwoord met ja of neen")
+if ja_neen == "neen":
+    your_number1 = float(input("geef mij het correcte nummer: "))
+    your_number2 = float(input("geef mij het correcte nummer: "))
+else: print("Dat is goed")
+
+bewerking = input("wat wil je doen? \n + , - , *, of / ? ")
+if bewerking == "+":
+    print(your_number1 + your_number2)
+elif bewerking == "-":
+    print(your_number1 - your_number2)
+elif bewerking == "*":
+    print(your_number1 * your_number2)
+elif bewerking == "/":
+    print(your_number1 / your_number2)
+else:
+    print("verkeerde wiskundige formule")
+os.system('pause')
